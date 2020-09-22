e5228a963db83677f6485425aa1d27d816c637fa
Dharamvir Saini <honeysaini88@gmail.com>
2020-09-22 09:43:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@
       stream.removeEventListener('addtrack', publish);
       publisher = OT.initPublisher('publisher', {
         videoSource: null,
-        audioSource: audioTracks[0],
+        audioSource: null,
         fitMode: 'contain',
         width: 320,
         height: 240
@@ -38,7 +38,7 @@
           video.pause();
           alert(err.message);
         } else {
-          //video.play();
+          video.play();
         }
       });
       
