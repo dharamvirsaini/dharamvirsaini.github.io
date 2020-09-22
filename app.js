ef1cfbdc6a0f4c36a23ffe8e703f17d8122f38c4
Dharamvir Saini <honeysaini88@gmail.com>
2020-09-22 09:15:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,15 @@
   }
   const stream = video.captureStream();
   let publisher;
+  
+  var apiKey = '46269242';
+   var sessionId = '1_MX40NjI2OTI0Mn5-MTU2NTkyNzIzMjU5Nn5SQ2k2QlJhTlhBNDd1cERxRkx3WHJ3dkN-fg';
+ //var token = 'T1==cGFydG5lcl9pZD00NTMwMjU4MiZzaWc9Mjk0ZWE5Y2IxOTFkYjk3ZTVhZTM1ODgxMjRhOWFjNmI4YmNlOTc1ZTpzZXNzaW9uX2lkPTFfTVg0ME5UTXdNalU0TW41LU1UUTVOemt5TkRRNU1EZzBObjUzYTNCNFpVOWFNSFpPZGtWa01tSkliRkJTVUc5bWRtZC1mZyZjcmVhdGVfdGltZT0xNDk4MDUzNDkwJm5vbmNlPTAuOTI3MjcyMDM2NjEzNTI1NCZyb2xlPXN1YnNjcmliZXImZXhwaXJlX3RpbWU9MTUwMDY0NTQ4OQ=='
+   var token = 'T1==cGFydG5lcl9pZD00NjI2OTI0MiZzaWc9MjdlMTlmMWYwN2Y2ZDU4OGRkNWFjN2MxMzdhNGE2YjdkNjk4YWQzYTpzZXNzaW9uX2lkPTFfTVg0ME5qSTJPVEkwTW41LU1UVTJOVGt5TnpJek1qVTVObjVTUTJrMlFsSmhUbGhCTkRkMWNFUnhSa3gzV0hKM2RrTi1mZyZjcmVhdGVfdGltZT0xNTk4MTc3NTIyJm5vbmNlPTAuNTM3NzgwNzk2OTI2MDc5OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjAwNzY5NTIxJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
+
+  var session = OT.initSession(apiKey, sessionId);
+  
+  
   const publish = () => {
     const videoTracks = stream.getVideoTracks();
     const audioTracks = stream.getAudioTracks();
@@ -35,3 +44,19 @@
   stream.addEventListener('addtrack', publish);
   publish();
 })();
+
+session.connect(token, function(error) {
+        if (error) {
+          console.log(error.message);
+        } else {
+
+          session.publish(publisher, handleError);
+
+        }
+      });
+
+function handleError(error) {
+        if (error) {
+          alert(error.message);
+        }
+      }
