3a333cc7ff58c8b211fab3b5cf85058c8db795d8
Dharamvir Saini <honeysaini88@gmail.com>
2020-09-22 09:48:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@
     if (!publisher && videoTracks.length > 0 && audioTracks.length > 0) {
       stream.removeEventListener('addtrack', publish);
       publisher = OT.initPublisher('publisher', {
-        videoSource: null,
+        videoSource: videoTracks[0],
         audioSource: audioTracks[0],
         fitMode: 'contain',
         width: 320,
