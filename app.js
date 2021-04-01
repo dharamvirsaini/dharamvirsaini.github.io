09ea938f386b42d8deacd092c7eb4772082e9896
Dharamvir Saini <honeysaini88@gmail.com>
2021-04-01 18:21:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,16 @@
     return;
   }
   const stream = video.captureStream();
+  
+  var apiKey = '46269242';
+  var sessionId = '1_MX40NjI2OTI0Mn5-MTYxMjg3NzUxOTM5OX5hckl3dTRJZ2JXQ0R0a2xZQnBURmpnS1d-fg';
+  //var token = 'T1==cGFydG5lcl9pZD00NTMwMjU4MiZzaWc9Mjk0ZWE5Y2IxOTFkYjk3ZTVhZTM1ODgxMjRhOWFjNmI4YmNlOTc1ZTpzZXNzaW9uX2lkPTFfTVg0ME5UTXdNalU0TW41LU1UUTVOemt5TkRRNU1EZzBObjUzYTNCNFpVOWFNSFpPZGtWa01tSkliRkJTVUc5bWRtZC1mZyZjcmVhdGVfdGltZT0xNDk4MDUzNDkwJm5vbmNlPTAuOTI3MjcyMDM2NjEzNTI1NCZyb2xlPXN1YnNjcmliZXImZXhwaXJlX3RpbWU9MTUwMDY0NTQ4OQ=='
+  var token = 'T1==cGFydG5lcl9pZD00NjI2OTI0MiZzaWc9MDg4MDc4Zjg4MTg1YTdlZDg0MGVhYzU2ZmQ1OGI3ZjdjOWE5OTg3OTpzZXNzaW9uX2lkPTFfTVg0ME5qSTJPVEkwTW41LU1UWXhNamczTnpVeE9UTTVPWDVoY2tsM2RUUkpaMkpYUTBSMGEyeFpRbkJVUm1wblMxZC1mZyZjcmVhdGVfdGltZT0xNjEyODc3NTMzJm5vbmNlPTAuNzc2NjExMTMwMzIyNzI5NCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjE1NDY5NTMyJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
+
+  
   let publisher;
+  var session = OT.initSession(apiKey, sessionId);
+  
   const publish = () => {
     const videoTracks = stream.getVideoTracks();
     const audioTracks = stream.getAudioTracks();
@@ -25,6 +34,14 @@
           alert(err.message);
         } else {
           video.play();
+          session.connect(token, function(error) {
+        if (error) {
+          console.log(error.message);
+        } else {
+          session.publish(publisher, handleError);
+        }
+      });
+
         }
       });
       publisher.on('destroyed', () => {
