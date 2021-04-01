a274217eccd1abe7fec583c3ba313f9e57efb0e8
Dharamvir Saini <honeysaini88@gmail.com>
2021-04-01 18:14:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,30 +8,10 @@
   }
   const stream = video.captureStream();
   let publisher;
-  
-    var apiKey = '46269242';
-  var sessionId = '1_MX40NjI2OTI0Mn5-MTYxMjg3NzUxOTM5OX5hckl3dTRJZ2JXQ0R0a2xZQnBURmpnS1d-fg';
-  //var token = 'T1==cGFydG5lcl9pZD00NTMwMjU4MiZzaWc9Mjk0ZWE5Y2IxOTFkYjk3ZTVhZTM1ODgxMjRhOWFjNmI4YmNlOTc1ZTpzZXNzaW9uX2lkPTFfTVg0ME5UTXdNalU0TW41LU1UUTVOemt5TkRRNU1EZzBObjUzYTNCNFpVOWFNSFpPZGtWa01tSkliRkJTVUc5bWRtZC1mZyZjcmVhdGVfdGltZT0xNDk4MDUzNDkwJm5vbmNlPTAuOTI3MjcyMDM2NjEzNTI1NCZyb2xlPXN1YnNjcmliZXImZXhwaXJlX3RpbWU9MTUwMDY0NTQ4OQ=='
-  var token = 'T1==cGFydG5lcl9pZD00NjI2OTI0MiZzaWc9NmRkYmNhNjYzYWUzOWQ1ZjUxMDA3M2NjZTRhN2RjOWUzNWM5ZmRhMzpzZXNzaW9uX2lkPTFfTVg0ME5qSTJPVEkwTW41LU1UWXhNamczTnpVeE9UTTVPWDVoY2tsM2RUUkpaMkpYUTBSMGEyeFpRbkJVUm1wblMxZC1mZyZjcmVhdGVfdGltZT0xNjE3MjgwMjEzJm5vbmNlPTAuNjUzMjEyODc3NTQ4NjA5NSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjE5ODcyMjEyJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
-
-  let session = OT.initSession(apiKey, sessionId);
-  
-  function handleError(error) {
-        if (error) {
-          alert(error.message);
-        }
-      }
-  
-  function playVideo() {
-      //  publisher = OT.initPublisher('myPublisherDiv', publishOptions);
-        //publisher.publishVideo(true);
-    video.play();
-      }
-  
   const publish = () => {
     const videoTracks = stream.getVideoTracks();
     const audioTracks = stream.getAudioTracks();
-    if (!publisher && audioTracks.length > 0) {
+    if (!publisher && videoTracks.length > 0 && audioTracks.length > 0) {
       stream.removeEventListener('addtrack', publish);
       publisher = OT.initPublisher('publisher', {
         videoSource: videoTracks[0],
@@ -44,11 +24,9 @@
           video.pause();
           alert(err.message);
         } else {
-        // video.play();
+          video.play();
         }
       });
-      
-      
       publisher.on('destroyed', () => {
         video.pause();
       });
@@ -56,20 +34,4 @@
   };
   stream.addEventListener('addtrack', publish);
   publish();
-  
-  session.connect(token, function(error) {
-        if (error) {
-          console.log(error.message);
-        } else {
-
-          session.publish(publisher, handleError);
-
-        }
-      });
-
-
-
-  
-  
 })();
-
